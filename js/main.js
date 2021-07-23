b1dd17c2e8151e831f4b93c40f9ea12d9bf12213
vhchauu <vuhachau2412@gmail.com>
2021-07-23 09:03:10 +0700

    commit

---
+++ js/main.js
@@ -0,0 +1,96 @@
+/*==================== DISPLAY MENU ====================*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+// if(navToggle){
+//     navToggle.addEventListener('click', () =>{
+//         navMenu.classList.add('show-menu')
+//     })
+// }
+
+navToggle.onclick = () => {
+    navMenu.classList.add('show-menu')
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+// if(navClose){a
+//     navClose.addEventListener('click', () =>{
+//         navMenu.classList.remove('show-menu')
+//     })
+// }
+
+navClose.onclick = () => {
+    navMenu.classList.remove('show-menu')
+}
+
+/*==================== REMOVE MENU PHONE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 100) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header')
+    }
+}
+
+/*==================== DISPLAY SKILL-ROADMAP ====================*/
+
+window.addEventListener('scroll', scrollHeader)
+
+const skillsContent = document.getElementsByClassName('skill_content');
+const skillsHeader = document.querySelectorAll('.skill_header')
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+    if (itemClass === 'skill_content skills_close') {
+        this.parentNode.className = 'skill_content skills_open'
+    }
+    if (itemClass === 'skill_content skills_open') {
+        this.parentNode.className = 'skill_content skills_close'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
+
+/*==================== SCROLLREVEAL ====================*/
+
+const sr = ScrollReveal({
+    distance: '30px',
+    duration: 1800,
+    reset: true,
+});
+
+sr.reveal(`.intro_content, .skill_container`, {
+    origin: 'top',
+    interval: 200,
+})
+
+sr.reveal(`.intro_picture, .second_content, .test_opener, .form_footer, .test_opener_profile `, {
+    origin: 'right',
+    interval: 200,
+})
+
+sr.reveal(`.second_picture, .pa, .roadmap1, .roadmap2, .lastest_result`, {
+    origin: 'left',
+    interval: 200,
+})
